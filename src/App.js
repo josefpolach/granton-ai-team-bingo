796dbc9baa88f0f987f49917b5a50e619d4d6f9f
Josef Polach <josef.polach@granton.cz>
2025-05-02 14:49:04 +1000

    feat: second app version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BingoApp from './components/BingoApp';
+
+function App() {
+    return (
+        <div className="App">
+            <BingoApp />
+        </div>
+    );
+}
+
+export default App;
